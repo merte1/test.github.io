f0cf7464a93e995f4c5748346827caaa88d5f1f1
merte1 <116719792+merte1@users.noreply.github.com>
2022-11-14 15:52:33 +0300

    Update IAT-TRR.js

--- IAT-TRR.js
+++ IAT-TRR.js
@@ -1,8 +1,7 @@
 //YBYB:Created from iat8.js, for Qualtrics
 define(['pipAPI','pipScorer','underscore'], function(APIConstructor, Scorer, _) {
 
-	/**
-	Created by: Yoav Bar-Anan (baranan@gmail.com). Modified by Gal
+	
 	 * @param  {Object} options Options that replace the defaults...
 	 * @return {Object}         PIP script
 	**/
