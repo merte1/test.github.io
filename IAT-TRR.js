090c320523c87fff66cde11e813f1ad34e785862
merte1 <116719792+merte1@users.noreply.github.com>
2022-11-14 16:44:05 +0300

    Update IAT-TRR.js

--- IAT-TRR.js
+++ IAT-TRR.js
@@ -2,6 +2,7 @@
 define(['pipAPI','pipScorer','underscore'], function(APIConstructor, Scorer, _) {
 
 	/**
+	Created by: Yoav Bar-Anan (baranan@gmail.com). Modified by Gal
 	 * @param  {Object} options Options that replace the defaults...
 	 * @return {Object}         PIP script
 	**/
