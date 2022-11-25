970d98f9852737caab26b4498868c224219028a3
merte1 <116719792+merte1@users.noreply.github.com>
2022-11-25 11:24:11 +0300

    Update IAT-TRR.js

--- IAT-TRR.js
+++ IAT-TRR.js
@@ -326,10 +326,10 @@ define(['pipAPI','pipScorer','underscore'], function(APIConstructor, Scorer, _) 
 			//attribute1, and attribute2 will be replaced with the name of attribute1 and attribute2.
 			//categoryA is the name of the category that is found to be associated with attribute1,
 			//and categoryB is the name of the category that is found to be associated with attribute2.
-			fb_strong_Att1WithCatA_Att2WithCatB : 'Cevaplarınız categoryB ile attribute2 ve categoryA ile attribute1 ilişkilendiren <b>güçlü düzeyde</b> bilinçsiz önyargıya sahipsiniz.',
-			fb_moderate_Att1WithCatA_Att2WithCatB : 'Cevaplarınız categoryB ile attribute2 ve categoryA ile attribute1 ilişkilendiren <b>orta düzeyde</b> bilinçisz önyargıya sahipsiniz.',
-			fb_slight_Att1WithCatA_Att2WithCatB : 'Cevaplarınız categoryB ile attribute2 ve categoryA ile attribute1 ilişkilendiren <b>az düzeyde</b> bilinçsiz önyargıya sahipsiniz.',
-			fb_equal_CatAvsCatB : 'Cevaplarınız categoryA ile attribute1 ve categoryB ile attribute2 ilişkilendiren önyargıya sahip değilsiniz.',
+			fb_strong_Att1WithCatA_Att2WithCatB : '<div> Cevaplarınız categoryB ile attribute2 ve categoryA ile attribute1 ilişkilendiren <b>güçlü düzeyde</b> bilinçsiz önyargıya sahipsiniz. </div>',
+			fb_moderate_Att1WithCatA_Att2WithCatB : '<div> Cevaplarınız categoryB ile attribute2 ve categoryA ile attribute1 ilişkilendiren <b>orta düzeyde</b> bilinçisz önyargıya sahipsiniz.</div>',
+			fb_slight_Att1WithCatA_Att2WithCatB : '<div> Cevaplarınız categoryB ile attribute2 ve categoryA ile attribute1 ilişkilendiren <b>az düzeyde</b> bilinçsiz önyargıya sahipsiniz.</div>',
+			fb_equal_CatAvsCatB : '<div> Cevaplarınız categoryA ile attribute1 ve categoryB ile attribute2 ilişkilendiren önyargıya sahip değilsiniz. </div>',
 
 			//Error messages in the feedback
 			manyErrors: 'Çok fazla yanlış olduğu için ölçüm yapılamadı.',
